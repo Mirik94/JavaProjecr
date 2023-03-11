68d74229432c7fe83504626c3879bf934c45acb3
Mirik94 <sharafov.mirgolib@yahoo.com>
2023-03-11 17:05:26 +0500

    second commit with mira file

---
+++ mira.java
@@ -0,0 +1,20 @@
+package devops_java_project;
+
+public class mira {
+	
+	public void add(int a, int b) {
+		int result = a + b;
+		
+		System.out.println("Result of additon: " + result);
+		
+		
+	}
+
+	public static void main(String[] args) {
+		mira obj = new mira();
+		obj.add(25, 55);
+		
+
+	}
+
+}
